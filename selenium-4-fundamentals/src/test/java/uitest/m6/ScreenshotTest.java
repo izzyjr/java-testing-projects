9fceef2c04c35b42fcb3c165c7087243e86c391f
Israel Mesa <mesa.israel50@gmail.com>
2023-06-22 21:00:01 -0400

    ScreenshotTest

---
+++ selenium-4-fundamentals/src/test/java/uitest/m6/ScreenshotTest.java
@@ -0,0 +1,54 @@
+package uitest.m6;
+
+import helper.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static helper.Pages.HOME;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+public class ScreenshotTest {
+
+    WebDriver driver;
+    
+    @Test
+    public void fullPageScreenShot() throws IOException {
+        driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+
+        File screenshot = ts.getScreenshotAs(OutputType.FILE);
+        Path destination = Paths.get("failure-screenshot.png");
+
+        Files.move(screenshot.toPath(), destination, REPLACE_EXISTING);
+    }
+
+    @Test
+    public void elementScreenshot() throws IOException {
+        driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        var form = driver.findElement(By.tagName("form"));
+
+        File screenshot = form.getScreenshotAs(OutputType.FILE);
+        Path destination = Paths.get("failure-screenshot.png");
+
+        Files.move(screenshot.toPath(), destination, REPLACE_EXISTING);
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        driver.quit();
+    }
+}
