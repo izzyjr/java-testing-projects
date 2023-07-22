5ea48e8fcc412c9d350b5277a4885703ef4d1e53
Israel Mesa <mesa.israel50@gmail.com>
2023-07-22 15:38:31 -0400

    HeadlessModeDemo

---
+++ selenium-4-fundamentals/src/test/java/uitest/m7/HeadlessModeDemo.java
@@ -0,0 +1,29 @@
+package uitest.m7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static helper.Pages.HOME;
+
+public class HeadlessModeDemo {
+
+    @Test
+    public void headlessDemo() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless=true");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(HOME);
+
+        WebElement button = driver.findElement(By.id("register"));
+        Assert.assertEquals(button.getText(), "Register");
+
+        driver.quit();
+    }
+}
