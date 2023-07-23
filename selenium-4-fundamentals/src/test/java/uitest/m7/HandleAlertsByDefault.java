e54f6a7e1d41a74b8aad5ba49029007a8797f626
Israel Mesa <mesa.israel50@gmail.com>
2023-07-23 15:29:40 -0400

    HandleAlertsByDefault

---
+++ selenium-4-fundamentals/src/test/java/uitest/m7/HandleAlertsByDefault.java
@@ -0,0 +1,29 @@
+package uitest.m7;
+
+import helper.DemoHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import static helper.Pages.HOME;
+
+public class HandleAlertsByDefault {
+
+    @Test
+    public void handleAlertsByDefault() {
+        ChromeOptions options = new ChromeOptions();
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.DISMISS);
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(HOME);
+
+        driver.findElement(By.id("clear")).click();
+        driver.findElement(By.id("register")).click();
+        DemoHelper.pause();
+        driver.quit();
+    }
+
+}
