600ddf8bb629ac14381b92c903615ae15f7e699b
Israel Mesa <mesa.israel50@gmail.com>
2023-12-18 22:15:19 -0500

    pom implementation - home/savings page

--- selenium-4-fundamentals/src/test/java/uitest/m6/WindowManagementDemo.java
+++ selenium-4-fundamentals/src/test/java/uitest/m6/WindowManagementDemo.java
@@ -3,23 +3,33 @@ package uitest.m6;
 import base.BaseTestClass;
 import org.openqa.selenium.WindowType;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.Home;
+import pages.Savings;
 
-import static helper.Pages.HOME;
-import static helper.Pages.SAVINGS;
 import static wait.WaitingUtils.pause;
 
 public class WindowManagementDemo extends BaseTestClass {
 
+    private Home homePage;
+    private Savings savingsPage;
+
+    @BeforeMethod
+    private void pageSetUp() {
+        homePage = Home.homePage(driver);
+        savingsPage = Savings.savingsPage(driver);
+    }
+
     @Test
-    public void tabsAndWindowsDemo() {
-        driver.get(HOME);
+    private void tabsAndWindowsDemo() {
+        homePage.goTo();
 
         String firstTab = driver.getWindowHandle();
 
         pause();
         driver.switchTo().newWindow(WindowType.TAB);
-        driver.get(SAVINGS);
+        savingsPage.goTo();
         Assert.assertEquals(driver.getWindowHandles().size(), 2);
 
         driver.switchTo().window(firstTab);
