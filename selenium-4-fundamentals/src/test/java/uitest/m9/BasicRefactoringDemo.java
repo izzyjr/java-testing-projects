31430d335b3d222a182799e5a7ce2a069caea528
Israel Mesa <mesa.israel50@gmail.com>
2024-08-10 12:45:50 -0400

    nit - static method import

--- selenium-4-fundamentals/src/test/java/uitest/m9/BasicRefactoringDemo.java
+++ selenium-4-fundamentals/src/test/java/uitest/m9/BasicRefactoringDemo.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import wait.WaitingUtils;
 
 import java.time.Duration;
 
